afc0c8055b1583ba98a734f4647d063fc69bada1
nickita-piter@rambler.ru <nickita-piter@rambler.ru>
2022-10-05 15:34:47 +0300

    Lesson_2_finished

---
+++ CardStorageService/PasswordUtilsClient/Program.cs
@@ -0,0 +1,6 @@
+﻿using CardStorageService.Utils;
+
+var result = PasswordUtils.CreatePasswordHash("12345");
+Console.WriteLine(result.passwordSalt);
+Console.WriteLine(result.passwordHash);
+Console.ReadKey(true);
