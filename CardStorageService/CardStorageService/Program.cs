51c0b51ced6a4c7572f8583f335b2f60ca0d6c6f
nickita-piter@rambler.ru <nickita-piter@rambler.ru>
2022-10-06 15:15:04 +0300

    Lesson_3_finished

--- CardStorageService/CardStorageService/Program.cs
+++ CardStorageService/CardStorageService/Program.cs
@@ -2,9 +2,13 @@ using CardStorageService.Data;
 using CardStorageService.Models;
 using CardStorageService.Services.Impl;
 using CardStorageService.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NLog.Web;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,11 +44,60 @@ builder.Host.ConfigureLogging(logging =>
 
 builder.Services.AddScoped<IClientRepositoryService, ClientRepository>();
 builder.Services.AddScoped<ICardRepositoryService, CardRepository>();
+builder.Services.AddSingleton<IAuthenticateService, AuthenticateService>();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme =
+    JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme =
+    JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new
+        TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthenticateService.SecretKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CardStorageService", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Description = "JWT Authorization header using the Bearer scheme(Example: 'Bearer 12345abcdef')",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                     Type = ReferenceType.SecurityScheme,
+                     Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+
+});
 
 var app = builder.Build();
 
@@ -54,9 +107,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseRouting();
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
+app.UseHttpLogging();
 
 app.Run();
---
+++ CardStorageService/CardStorageService/Services/Interfaces/IAuthenticateService.cs
@@ -0,0 +1,13 @@
+﻿using CardStorageService.Models;
+using CardStorageService.Models.Requests;
+using CardStorageService.Models.Responses;
+
+namespace CardStorageService.Services.Interfaces
+{
+    public interface IAuthenticateService
+    {
+        AuthenticationResponse Login(AuthenticationRequest authenticationRequest);
+
+        public SessionInfo GetSessionInfo(string sessionToken);
+    }
+}
